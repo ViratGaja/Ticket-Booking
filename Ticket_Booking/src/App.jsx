8effd4d74028dd8b77a3f8aa2e470d8883da2b0b
Gajapathi K <113296010+ViratGaja@users.noreply.github.com>
2025-07-14 22:02:03 +0530

    learn

--- Ticket_Booking/src/App.jsx
+++ Ticket_Booking/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home'
+import Movies from './Pages/Movies'
+import MovieDetails from './Pages/MovieDetails'
+import SeatLayout from './Pages/SeatLayout'
+import MyBooking from './Pages/MyBooking'
+import Favorite from './Pages/Favorite'
 
 const App = () => {
+
+  const isAdminRoute=useLocation().pathname.startsWith('/admin')
   return (
-    <div>
-      
-    </div>
+    <>
+      {!isAdminRoute &&<Navbar />}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/mmovies/:id' element={<MovieDetails/>}/>
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+        <Route path='/my-booking' element={<MyBooking/>}/>
+        <Route path='/Favorite' element={<Favorite/>}/>
+      </Routes>
+    </>
   )
 }
 
